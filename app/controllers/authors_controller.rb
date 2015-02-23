bd9275eb955a1ff077569dd7155653868353c871
Paris Jones <bayarette@gmail.com>
2015-02-23 00:35:12 -0800

    Functioning skeleton with no styling

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,14 +3,49 @@ class AuthorsController < ApplicationController
   	@authors=Author.all
   end
 
-  def show
-  	@author=Author.find params[:id]
-  	render :show
-  end
 
   def new
+  	@author=Author.new   
+  end
+
+
+  def show
+  	@author=Author.find params[:id]
   end
 
   def edit
+    @author=Author.find params[:id]
   end
+
+
+  def create
+    @author=Author.new author_params
+    if @author.save
+      redirect_to authors_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author=Author.find params[:id]
+    if @author.update_attributes author_params
+      redirect_to author_path(@author)
+    else 
+      render :edit
+    end
+  end
+
+
+  def destroy
+    author=Author.find params[:id]
+    author.destroy
+    redirect_to authors_path
+  end
+
+private
+def author_params
+	params.require(:author).permit(:first_name, :last_name, :age)
+end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-  	@posts=Post.all
+  	@author = Author.find params[:author_id]
+  	@posts = @author.posts
   end
 
   def show
@@ -9,8 +10,46 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@author=Author.find params[:author_id]
+  	@post=Post.new
   end
 
   def edit
+  	@post=Post.find params[:id]
+  	@author=@post.author
+  end
+
+  def create
+  	@author=Author.find params[:author_id]
+  	@post=Post.new post_params
+  	@post.author = @author
+  	if @post.save
+  		redirect_to author_posts_path(@author)
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	@post=Post.find params[:id]
+  	@post.update_attributes post_params
+  	@author = @post.author
+  	if @post.save
+  		redirect_to post_path(@post)
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	post=Post.find params[:id]
+  	post.destroy
+  	
+  	redirect_to author_posts_path(post.author)
+  end
+
+  private
+  def post_params
+  	params.require(:post).permit(:title, :body, :category, :author_id)
   end
 end
